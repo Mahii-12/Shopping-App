1db1edf685e0736fd8d536f1a43b6bd2c45baea0
Eeralla Mahender <112060996+Mahii-12@users.noreply.github.com>
2024-04-06 19:24:48 +0530

    Update Coupon.java

--- eclipse-workspace/Shopping-App/src/main/java/com/example/demo/model/Coupon.java
+++ eclipse-workspace/Shopping-App/src/main/java/com/example/demo/model/Coupon.java
@@ -3,9 +3,11 @@ package com.example.demo.model;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name="Coupon")
 public class Coupon {
 
-	public static final String OFF5_CODE = "OFF5";
+    public static final String OFF5_CODE = "OFF5";
     public static final String OFF10_CODE = "OFF10";
 
     public static final int OFF5_DISCOUNT_PERCENTAGE = 5;
