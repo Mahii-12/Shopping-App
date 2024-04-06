a8a8bb571edf761c1824e22e620ad2fe3591841e
Eeralla Mahender <112060996+Mahii-12@users.noreply.github.com>
2024-04-06 18:23:42 +0530

    Delete eclipse-workspace/E-Commerce-Platform-App directory

--- eclipse-workspace/E-Commerce-Platform-App/src/main/java/com/example/demo/repository/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Order;
-
-public interface OrderRepo extends JpaRepository<Order,Long> {
-
-}
